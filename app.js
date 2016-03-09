0a3af1ce84fe9c2f4c692af0b8e3274a2051e585
Daniel Craig <daniel@danielcraig.me>
2016-03-09 16:09:28 +0000

    added app detail page

--- app.js
+++ app.js
@@ -2,10 +2,27 @@
 	$(function(){
 
 
-		$('*[data-load-url]').click(function(e){
+		$('ul[for="projects-menu"] *[data-load-url]').click(function(e){
 			$('#jenca-content').load($(this).attr('data-load-url'), function(response, status, xhr){
 				if(status == "error")
 					$('#jenca-content').html('<h3>Page Not Found</h3>');
+			});
+		});
+
+		$('#jenca-content').on('click',  '#project-installed-app-list *[data-load-url]', function(e){
+			if($('#app-content').attr('data-positioned'))
+				$('#app-content').animate({left:$('body').outerWidth()}, 400, 'swing');
+			else
+				$('#app-content').css({left:$('body').outerWidth(), top:(-1 * $('#jenca-content').outerHeight())});
+
+
+			$('#app-content').load($(this).attr('data-load-url'), function(response, status, xhr){
+				if(status == "error")
+					$('#app-content').html('<h3>Page Not Found</h3>');
+				else
+					$('#app-content').animate({left:(($('#jenca-content').outerWidth()- $('#jenca-content').width())/2)+75});
+
+				$('#app-content').attr('data-positioned', 'positioned');
 			});
 		});
 
